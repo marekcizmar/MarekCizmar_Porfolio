02eba710106a68c40f76df72950d56d1aecb3243
Marek Cizmar <marekcizmar@outlook.sk>
2022-11-14 20:28:33 +0100

    Added navbar animation

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ let button = document.getElementById("hamburger"),
     cross = document.getElementById("xmark"),
     mainNav = document.getElementById("mainNav");
 
-button.addEventListener("click", () => {
-    mainNav.classList.add("hidden");
-    navigation.classList.remove("hidden");
-});
+    
+let isOpened = false;
 
-cross.addEventListener("click", () => {
-    mainNav.classList.remove("hidden");
-    navigation.classList.add("hidden");
-});
+function openDropDown(){
+    isOpened = !isOpened;
+    if(isOpened){
+        navigation.classList.remove("dropdown-close");
+        navigation.classList.add("dropdown-open");
+    }else{
+        navigation.classList.remove("dropdown-open");
+        navigation.classList.add("dropdown-close");
+    }
+}
 
 let tabs = document.querySelectorAll('.tabs_toggle');
 let contents = document.querySelectorAll('.tabs_content');
